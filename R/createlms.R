01cd0c58dafa775da1013307c2dc960c3a144403
mvogel78 <mandy.vogel@googlemail.com>
2017-07-01 11:18:48 +0200

    remove user accessable documentation of unexported functions

--- R/createlms.R
+++ R/createlms.R
@@ -87,26 +87,27 @@ select_meas <- function(data, subject = "subject", prop = 1, verbose = F){
     data
 }
 
-##' fit lms
-##'
-##' wrapper around the \code{\link[gamlss]{lms}} function in the gamlss package
-##' returns the fitted lms-parameter at given age points
-##' the function is called inside \code{\link{do_iterations}} and may not called directly
-##' @title fit lms
-##' @param data dataframe as return by select_meas()
-##' @param age.min lower bound of age
-##' @param age.max upper bound of age
-##' @param age.int stepwidth of the age variable
-##' @param dist distribution used for the fitting process, has to be one of BCCGo, BCPEo, BCTo as they are accepted by lms()
-##' @param mu.df degree of freedem location parameter
-##' @param sigma.df degree of freedem spread parameter
-##' @param nu.df degree of freedem skewness parameter
-##' @param tau.df degree of freedem kurtosis parameter
-##' @param value names of the value variable (character) if different from value, ignored
-##' @return list containing a dataframe of the fitted lms parameter at the given age points and the fitted model
-##' @author Mandy Vogel
-fit_gamlss <- function(data, age.min = 0.25, age.max = 18, age.int = 1/12, dist = "BCCGo",
-                       mu.df = 4,sigma.df = 3, nu.df = 2, tau.df = 2, value){
+## fit lms
+##
+## wrapper around the \code{\link[gamlss]{lms}} function in the gamlss package
+## returns the fitted lms-parameter at given age points
+## the function is called inside \code{\link{do_iterations}} and may not called directly
+## @title fit lms
+## @param data dataframe as return by select_meas()
+## @param age.min lower bound of age
+## @param age.max upper bound of age
+## @param age.int stepwidth of the age variable
+## @param keep.models indicator whether or not models in each iteration should be kept
+## @param dist distribution used for the fitting process, has to be one of BCCGo, BCPEo, BCTo as they are accepted by lms()
+## @param mu.df degree of freedem location parameter
+## @param sigma.df degree of freedem spread parameter
+## @param nu.df degree of freedem skewness parameter
+## @param tau.df degree of freedem kurtosis parameter
+## @param value names of the value variable (character) if different from value, ignored
+## @return list containing a dataframe of the fitted lms parameter at the given age points and the fitted model
+## @author Mandy Vogel
+fit_gamlss <- function(data, age.min = 0.25, age.max = 18, age.int = 1/12, keep.models = F,
+                       dist = "BCCGo", mu.df = 4,sigma.df = 3, nu.df = 2, tau.df = 2, value){
     tr.obj <- try(mm <- gamlss::lms(value, age, data = data[,-grep("group",names(data))],
                             families = dist,method.pb = "ML", k = 2,trace = F,
                             mu.df = mu.df, sigma.df = sigma.df, nu.df = nu.df, tau.df = tau.df))
@@ -134,16 +135,26 @@ fit_gamlss <- function(data, age.min = 0.25, age.max = 18, age.int = 1/12, dist 
 ##' @param data.list list of dataframes as returned by prepare_data
 ##' @param prop.fam proportion of families to be sampled
 ##' @param prop.subject proportion of subject to be sampled
-##' @param verbose whether or not information about sampling will be printed during while iterate 
-##' @inheritParams fit_gamlss
+##' @param verbose whether or not information about sampling will be printed during while iterate
+##' @param age.min lower bound of age
+##' @param age.max upper bound of age
+##' @param age.int stepwidth of the age variable
+##' @param keep.models indicator whether or not models in each iteration should be kept
+##' @param dist distribution used for the fitting process, has to be one of BCCGo, BCPEo, BCTo as they are accepted by lms()
+##' @param mu.df degree of freedem location parameter
+##' @param sigma.df degree of freedem spread parameter
+##' @param nu.df degree of freedem skewness parameter
+##' @param tau.df degree of freedem kurtosis parameter
 ##' @return list of lists each containing a dataframe of the fitted lms parameter at the given age points and the fitted model
 ##' @author Mandy Vogel
 ##' @export
 one_iteration <- function(data.list, prop.fam = 0.75, prop.subject = 1, age.min = 0, age.max = 18, age.int = 1/12,
-                          dist = "BCCGo", sigma.df = 3, nu.df = 2, mu.df = 4, tau.df = 2, verbose = F){
+                          keep.models = F, dist = "BCCGo", sigma.df = 3, nu.df = 2, mu.df = 4, tau.df = 2, verbose = F){
     tmp.l <- lapply(data.list, select_fams, prop = prop.fam, verbose = verbose)
     tmp.l <- lapply(tmp.l, select_meas, prop = prop.subject, verbose = verbose)
-    lapply(tmp.l, fit_gamlss, dist = dist, sigma.df = sigma.df, nu.df = nu.df, mu.df = mu.df, tau.df = tau.df, age.min = age.min, age.max = age.max )
+    lapply(tmp.l, fit_gamlss, dist = dist, keep.models = keep.models,
+           sigma.df = sigma.df, nu.df = nu.df, mu.df = mu.df, tau.df = tau.df,
+           age.min = age.min, age.max = age.max )
 }
 
 ##' Do lms iterations 
