d0d462e49980181a723fc28a94edae54288ddfcc
mvogel78 <mandy.vogel@googlemail.com>
2017-09-06 21:17:56 +0200

    non-standard to standard eval in ggplot functions

--- R/misc.R
+++ R/misc.R
@@ -88,8 +88,8 @@ wormplot_gg <- function(m=NULL, residuals=NULL, age=NA, n.inter=1, y.limits = c(
     if(inherits(m,"gamlss"))
         residuals <- residuals(m)
     mm <- tibble::tibble(x = seq(-4,4,l = 1000),
-                         yu = 1.96 * sqrt(stats::pnorm(x)*(1-stats::pnorm(x))/length(residuals))/stats::dnorm(x),
-                         yl = -yu)
+                         yu = 1.96 * sqrt(stats::pnorm(.data$x)*(1-stats::pnorm(.data$x))/length(residuals))/stats::dnorm(.data$x),
+                         yl = -.data$yu)
     mm <- reshape2::melt(mm, id.var = "x")
     tmp <- data.frame(residuals = residuals,
                       age = age)
@@ -100,21 +100,21 @@ wormplot_gg <- function(m=NULL, residuals=NULL, age=NA, n.inter=1, y.limits = c(
         tmp$ag <- "all ages"
 
     }
-    tmp <- dplyr::group_by(tmp, ag) %>% tidyr::nest()
-    tmp <- dplyr::mutate(tmp, qq = purrr::map(data, function(x){
+    tmp <- dplyr::group_by(tmp, .data$ag) %>% tidyr::nest()
+    tmp <- dplyr::mutate(tmp, qq = purrr::map(.data$data, function(x){
         qq <- as.data.frame(stats::qqnorm(x$residuals, plot.it = F))
         qq$y <- qq$y - qq$x
         qq
     }))
-    tmp <- tidyr::unnest(tmp, qq)
-    ggplot2::ggplot(tmp, ggplot2::aes(x = x, y = y)) +
+    tmp <- tidyr::unnest(tmp, .data$qq)
+    ggplot2::ggplot(tmp, ggplot2::aes_string(x = "x", y = "y")) +
         ggplot2::geom_point(shape = 21, size = 1, colour = "#2171B5") +
         ggplot2::geom_line(data = mm, inherit.aes = F,
-                  ggplot2::aes(x = x, y = value, group = variable),
+                  ggplot2::aes_string(x = "x", y = "value", group = "variable"),
                   linetype = 2, colour = "forestgreen") +
         ggplot2::geom_vline(xintercept = 0, colour = "firebrick3", linetype = 4) +
         ggplot2::scale_y_continuous(limits = y.limits) +
-        ggplot2::facet_wrap( ~ ag) +
+        ggplot2::facet_wrap( ~ag) +
         ggplot2::labs(x = "Unit normal quantiles",
              y = "Deviation") +
         ggplot2::theme_minimal() +
